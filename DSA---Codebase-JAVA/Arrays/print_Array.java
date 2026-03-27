a0c91b19c70f07c86edec43545551f9faa5407fe
Abhishek Tiwari <abhishektiwarii2027@gmail.com>
2026-03-28 00:52:45 +0530

    Add files via upload

---
+++ DSA---Codebase-JAVA/Arrays/print_Array.java
@@ -0,0 +1,8 @@
+class print_Array{
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
